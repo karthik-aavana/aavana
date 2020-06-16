855758071531a0fb1d221e3b8b1b57d7c769aea9
shweta-aavana <62342323+shweta-aavana@users.noreply.github.com>
2020-06-16 15:31:19 +0530

    JS file

---
+++ assets/js/chat_box_script.js
@@ -0,0 +1,4 @@
+(function(){
+		var js,fs,d=document,id="tars-widget-script",b="https://tars-file-upload.s3.amazonaws.com/share/";if(!d.getElementById(id)){js=d.createElement("script");js.id=id;js.type="text/javascript";js.src=b+"js/widget.js";
+			fs=d.getElementsByTagName("script")[0];fs.parentNode.insertBefore(js,fs)}})();
+			window.tarsSettings={"convid":"Bklz3I","img":"assets/images/chatbot.png","autoopen":"5","class":"fab bounce closed"}
